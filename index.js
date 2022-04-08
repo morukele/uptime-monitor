18ee98a0c610f4132c4b9563787ef66bd43cf3d7
Oghenemarho Orukele <morukele@yahoo.com>
2022-04-08 20:06:28 +0200

    Handled request payload

--- index.js
+++ index.js
@@ -1,28 +1,64 @@
 /*
-* Primary file for the API
-*
-*/
+ * Primary file for the API
+ *
+ */
 
 // Dependencies
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 // The server should respond to all request with a string
-const server = http.createServer( (req, res) =>{
+const server = http.createServer((req, res) => {
+	// Get the URL and parse it
+	var parsedUrl = url.parse(req.url, true);
 
-  // Get the URL and parse it
-  var parsedUrl = url.parse(req.url, true)
+	// Get the path
+	var path = parsedUrl.pathname;
+	var trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
-  // Get the path
+	// Get the query string as an object
+	var queryStringObject = parsedUrl.query;
 
-  // Send the response
+	// Get the http method
+	var method = req.method.toLowerCase();
 
-  // Log the request path
+	// Get the headers as an object
+	var headers = req.headers;
 
-  res.end('Hello World\n')
-})
+	// Get the payload, if any
+	// Data comes in as a stream so we decode it and save it into the buffer object
+	var decoder = new StringDecoder("utf-8");
+	var buffer = "";
+	req.on("data", (data) => {
+		buffer += decoder.write(data);
+	});
+	req.on("end", () => {
+		buffer += decoder.end();
+
+		// Send the response
+		res.end("Hello World\n");
+
+		// Log the request path
+		console.log(buffer);
+	});
+});
 
 // Start th server, and have it listen on port 3000
 server.listen(3000, () => {
-  console.log("The server is listening on port 3000")
-})
+	console.log("The server is listening on port 3000");
+});
+
+//Define the request handlers
+const handlers = {};
+
+// Sample handler
+handlers.sample = (data, callback) => {};
+
+// Not found handler
+handlers.notFound = (data, callback) => {};
+
+// Define a request router
+const router = {
+	sample: handlers.sample,
+};
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Uptime Monitor
+An API that checks the uptime of a URL and sends an SMS to the user relaying the status of the URL
+
+The API is built using NodeJS native modules and no third party NPM library. 
